b2a78afaa6c2e603f1cd00de682b5955e531bc32
srinivaskrish <srinivas.krishnan@gmail.com>
2018-07-25 11:27:28 -0700

    sample 2 added and sample modified

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 	
 	public static void main(String[] args){
 		
-		System.out.println("This is a sample class");
+		System.out.println("This is a sample class, first updates");
 	}
 
 }
---
+++ src/sample/Sample2.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Sample2 {
+	
+public static void main(String[] args){
+		
+		System.out.println("This is a sample2 class");
+	}
+
+
+}
